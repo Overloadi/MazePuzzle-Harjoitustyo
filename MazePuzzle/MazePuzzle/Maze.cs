db989229149a130bb5e41a247a397b171c79736a
K1625 <K1625@PROJ-422-WS-02>
2017-03-21 14:20:53 +0200

    tehty alustavat luokat

---
+++ MazePuzzle/MazePuzzle/Maze.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazePuzzle
+{
+    class Maze
+    {
+        public List<Box> boxes;
+        public int rowCount;
+        public int columnCount;
+
+
+        public Maze(int[,] matrix)
+        {
+            bool passable;
+            for (int col = 0; col < matrix.GetLength(0); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(1); row++)
+                {
+                    if (matrix[col,row] == 1)
+                    {
+                        passable = true;
+                    }
+                    else
+                    {
+                        passable = false;
+                    }
+                    Box box = new Box(passable, row, col);
+                    boxes.Add(box);
+                }
+            }
+        }
+    }
+}
